327a60cca3a6cbcce995be76c07f389e33415244
Sherry1481 <shareyscorpi1@gmail.com>
2021-07-30 23:01:31 +0500

    done

--- Gods Project/TableViewController.swift
+++ Gods Project/TableViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    let greekGods = GreekGodsData().getAllGods()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    //Table view code begins
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return greekGods.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "greekGodsTableViewCell", for: indexPath)
+        
+        let god = greekGods[indexPath.row]
+        cell.textLabel!.text = god.name
+        cell.detailTextLabel!.text = "Power: \(god.superPower)"
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    //Table view code ends
+    
+    
 
 }
 
